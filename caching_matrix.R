d1e1b327ccff114069dd95e5f9edfd0c0c4b854d
ankit06kr <ankit06kr@gmail.com>
2016-06-06 11:56:05 +0530

    Update caching_matrix.R

--- caching_matrix.R
+++ caching_matrix.R
@@ -1,9 +1,3 @@
-# caching_matrix creates a list containing a function to
-# 1. set the value of the matrix
-# 2. get the value of the matrix
-# 3. set the value of inverse of the matrix
-# 4. get the value of inverse of the matrix
-
 caching_matrix <- function(x = matrix()) {
     invy <- NULL
     set <- function(y) {
@@ -18,13 +12,6 @@ caching_matrix <- function(x = matrix()) {
          setInverse = setInverse,
          getInverse = getInverse)
 }
-
-#The following function returns the inverse of the matrix. It first checks if
-# the inverse has already been computed. If so, it gets the result and skips the
-# computation. If not, it computes the inverse, sets the value in the cache via
-# setinverse function.
-
-# This function assumes that the matrix is always invertible.
 
 cacheSolve <- function(x, ...) {
     invy <- x$getinverse()
